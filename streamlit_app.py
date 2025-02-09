a6da28cd55eaf283eaf80b69ad6503eebdccf622
YashB1503 <139315977+YashB1503@users.noreply.github.com>
2025-02-09 19:26:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,8 +27,21 @@ with st.sidebar:
   st.header('Input Feature')
   #"species","island","bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g","sex"
   island = st.selectbox('Island', ('Torgersen', 'Biscoe', 'Dream'))
-  gender = st.selectbox('Gender', ('male', 'female'))
   bill_length_mm = st.slider('Bill Length (mm)', 32.1, 59.6, 43.9)
   bill_depth_mm = st.slider('Bill Depth (mm)', 13.1, 21.5, 17.5)
   flipper_length_mm = st.slider('Flipper Length (mm)', 172.0, 231.0, 201.0)
   body_mass_g = st.slider('Body Mass (gm)', 2700.0, 6300.0, 4207.0)
+  gender = st.selectbox('Gender', ('male', 'female'))
+
+  # Create a dataframe for the input feature
+  data = {
+    'island' : island,
+    'bill_length_mm' : bill_length_mm,
+    'bill_depth_mm' : bill_depth_mm,
+    'flipper_length_mm' : flipper_length_mm,
+    'body_mass_g' : body_mass_g,
+    'gender' : gender
+  }
+
+  input_df = pd.DataFrame(data, index = 0)
+  input_df
