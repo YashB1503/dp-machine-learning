f61a22ee53febf77b7bb5123f9502d68f7d4e008
YashB1503 <139315977+YashB1503@users.noreply.github.com>
2025-02-09 18:37:36 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,9 @@ import pandas as pd
 
 st.title('🎈 Machine Learning App')
 
-df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv")
-df
-
 st.write('This is app builds a machine learning model!')
+
+with st.expander('Data'):
+  st.write("**Raw Data")
+  df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv")
+  df
