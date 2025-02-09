a4acc4cf1d9c8110ceff472d222ee62bc984cc7b
YashB1503 <139315977+YashB1503@users.noreply.github.com>
2025-02-09 20:02:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,6 +61,10 @@ with st.sidebar:
 def target_encode(val):
   return target_mapper[val]
 
+y = y_raw.apply(target_encode)
+y
+
+
 with st.expander('Input Feature'):
   st.write('**X**')
   input_df
